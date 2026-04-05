fd9677d6ec9722b32ece654f576e157a03719cf5
Illendula Gokul <gokulillendula@gmail.com>
2026-04-05 18:13:44 +0530

    changed the generative model

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .views import get_recipe
 urlpatterns = [
-    path('get-recipe/', get_recipe, name='get_recipe'),
+        path('get-recipe/', get_recipe, name='get_recipe'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,54 +1,31 @@
-import requests
-from django.conf import settings
+from google import genai
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
 
-
-HF_URL = "https://router.huggingface.co/v1/chat/completions"
+client = genai.Client(api_key=settings.GEMINI_API_KEY)
 
 
 @api_view(["POST"])
 def get_recipe(request):
     ingredients = request.data.get("ingredients", [])
 
-    if not ingredients:
-        return Response({"error": "No ingredients provided"}, status=400)
+    if not ingredients or not isinstance(ingredients, list):
+        return Response({"error": "Invalid ingredients"}, status=400)
 
-    prompt = f"""
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients.
-
-You don't need to use every ingredient they mention in your recipe.
-The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients.
-
-Format your response in **Markdown** so it can be rendered on a web page.
-
-Here are the ingredients the user has:
-{', '.join(ingredients)}
-"""
-
-
-    headers = {
-        "Authorization": f"Bearer {settings.HF_TOKEN}",
-        "Content-Type": "application/json",
-    }
-
-    payload = {
-    "model": "meta-llama/Meta-Llama-3-8B-Instruct",
-    "messages": [
-        {"role": "user", "content": prompt}
-    ],
-    "max_tokens": 512,
-}
-
+    prompt = f"Suggest a recipe using: {', '.join(ingredients)}"
 
     try:
-        res = requests.post(HF_URL, headers=headers, json=payload)
-        data = res.json()
-        print("HF RESPONSE:", data)
-        recipe = data["choices"][0]["message"]["content"]
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
 
-        return Response({"recipe": recipe})
+        return Response({"recipe": response.text})
 
     except Exception as e:
+        print("ERROR:", e)
         return Response({"error": str(e)}, status=500)
+
+
 
